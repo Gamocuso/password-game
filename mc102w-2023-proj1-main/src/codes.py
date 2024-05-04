7defb4979b502ef7285132cd6f9a6c6e644d1223
Gamocuso <145231277+Gamocuso@users.noreply.github.com>
2024-05-03 22:39:20 -0300

    Update codes.py

--- mc102w-2023-proj1-main/src/codes.py
+++ mc102w-2023-proj1-main/src/codes.py
@@ -1,40 +1,91 @@
 #!/usr/bin/env python3
 """
-Contem o código secreto e a função para verificar o palpite.
-"""
+Implemente aqui o seu código para o jogador.
 
+Seu principal objetivo é implementar a função `player`, que deve retornar uma lista de 4 cores, o seu próximo palpite.
+Como exemplo, a função abaixo retorna um palpite aleatório.
+
+Dicas:
+- Você pode implementar outras funções para auxiliar a função `player`.
+- Você pode salvar informações entre os palpites usando variáveis globais (fora de qualquer função).
+"""
 from colors import *
 from random import sample
 
-# Lista de cores disponíveis
-COLORS = [RED, GREEN, BLUE, YELLOW, ORANGE, BLACK, WHITE]
-CODE_LENGTH = 4  # Tamanho do código secreto
-
-HIST = []  # Histórico de palpites
-RES = []  # Histórico de resultados
-CODE = sample(COLORS, CODE_LENGTH)  # Código secreto
+# Cores disponíveis para o palpite
+colors = [RED, GREEN, BLUE, YELLOW, ORANGE, BLACK, WHITE]
 
 
-def guess_code(guess):
+def player(guess_hist, res_hist):
     """
-    Função para verificar o palpite do jogador.
+    Função principal do jogador.
 
-    guessed_code: lista de 4 cores
+    Esta função deve retornar o seu palpite, que deve ser uma lista de 4 cores.
+    As cores disponíveis são: RED, GREEN, BLUE, YELLOW, ORANGE, BLACK, WHITE.
 
-    Retorna uma tupla com a quantidade de cores corretas e a quantidade de cores
-    na posição correta.
+    Parâmetros:
+    - guess_hist: lista de palpites anteriores
+    - res_hist: lista de resultados anteriores
+
+    Retorna:
+    - lista de 4 cores
+
+    Exemplo:
+    return [RED, GREEN, BLUE, YELLOW]
     """
-    correct_colors = sum(c in CODE for c in guess)
-    correct_positions = sum(c == g for c, g in zip(CODE, guess))
-
-    HIST.append(guess)
-    RES.append((correct_colors, correct_positions))
-
-    # Imprime o palpite e o resultado
-    if len(RES) < 10:
-        print(f"({len(RES)}):    ", *guess, f" => {RES[-1]}     ")
+    if len(guess_hist) == 0:
+        return [RED, GREEN, BLUE, YELLOW]
     else:
-        print(f"({len(HIST)}):   ", *guess, f" => {RES[-1]}     ")
+        return all_arrangements(4, colors, guess_hist, res_hist)
 
-    # Retorna o resultado
-    return RES[-1]
+def all_arrangements(len_list, elements_list, guess_hist, res_hist):
+
+    arrang = [0] * len_list
+
+    def add_elements(elements, index):
+
+        for i in elements:
+            arrang[index] = i
+            if index != len_list - 1:
+                new_list_elements = list(set(elements) - {i}).copy()
+                test = add_elements(new_list_elements, index=index + 1)
+                if test is None:
+                    continue
+                else:
+                    return test
+            else:
+                if test_validation(arrang, guess_hist, res_hist):
+                    return arrang
+                    
+
+    return add_elements(elements_list, 0)
+
+def test_validation(test, guess_hist, res_hist):
+
+    for i in range(len(guess_hist)):
+
+        if quantity_valid(test, guess_hist[i], res_hist[i]) and position_valid(test, guess_hist[i], res_hist[i]) and test != guess_hist[i]:
+            continue
+        else:
+            return False
+    
+    return True
+
+
+def quantity_valid(test, previous_test, res_test):
+    diff = 4 - len(list(set(previous_test) - set(test)))
+    return True if diff == res_test[0] else False
+
+def position_valid(test, previous_test, res_test):
+    qnt_positions = 0
+    if test[0] == previous_test[0]: 
+        qnt_positions +=1
+    if test[1] == previous_test[1]: 
+        qnt_positions +=1
+    if test[2] == previous_test[2]: 
+        qnt_positions +=1
+    if test[3] == previous_test[3]: 
+        qnt_positions +=1
+
+    return True if qnt_positions == res_test[1] else False
+    
